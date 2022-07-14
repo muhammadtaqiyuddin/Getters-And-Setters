32f37d84e8acd20f8906165fa4da019c6eb12b7c
mtaqiyuddin <mtaqiyuddin101@gmail.com>
2022-07-14 16:19:30 +0700

    Getters & Setters

---
+++ Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class Book
+    {
+        private string title;
+        public string author;
+        public int pages;
+        public Book(string aTitle,string aAuthor,int aPages)
+        {
+            Title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+        public string Title
+        {
+            get { return title; }
+            set { 
+                if(value == "G" || value == "PG" || value == "PG-13" || value == "R" )
+                {
+                    title = value;
+                }
+                else
+                {
+                    title = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Harry Potter", "JKRowling",400);
+            Book book2 = new Book("Lord Angga", "Tolkeir", 700);
+
+            // G, PG, PG-13, R, NR
+            book1.Title = "Rich Dad Poor Dad";
+            Console.WriteLine(book1.Title);
+
+            book2.Title = "G";
+            Console.WriteLine(book2.Title);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
